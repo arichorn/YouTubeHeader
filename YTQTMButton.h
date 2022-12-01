8cd63831ce5146fd0f766c5d7b1253c333481092
PoomSmart <thatchapon.unp@outlook.com>
2022-12-01 14:12:37 +0700

    Added things used by YouMute 1.1.0

--- YTQTMButton.h
+++ YTQTMButton.h
@@ -6,9 +6,12 @@
 + (instancetype)iconButton;
 + (instancetype)textButton;
 @property (nonatomic, assign, readwrite) CGFloat buttonImageTitlePadding;
+@property (nonatomic, assign, readwrite) CGFloat minHitTargetSize;
 @property (nonatomic, assign, readwrite) CGFloat verticalContentPadding;
 @property (nonatomic, assign, readwrite) NSInteger buttonLayoutStyle;
 @property (nonatomic, assign, readwrite) BOOL refreshRendererAfterPageStyling;
 @property (nonatomic, assign, readwrite) BOOL sizeWithPaddingAndInsets;
+@property (nonatomic, copy, readwrite) NSString *accessibilityIdentifier;
+@property (nonatomic, copy, readwrite) NSString *accessibilityLabel;
 - (void)setTitleTypeKind:(NSInteger)titleTypeKind typeVariant:(NSInteger)typeVariant;
 @end
--- YTSettingsSectionItem.h
+++ YTSettingsSectionItem.h
@@ -9,8 +9,11 @@
 @property (nonatomic, assign, readwrite) int settingItemId;
 @property (nonatomic, copy, readwrite) BOOL (^selectBlock)(YTSettingsCell *, NSUInteger);
 @property (nonatomic, copy, readwrite) BOOL (^switchBlock)(YTSettingsCell *, BOOL);
-+ (instancetype)itemWithTitle:(NSString *)title accessibilityIdentifier:(NSString *)accessibilityIdentifier detailTextBlock:(id)detailTextBlock selectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))selectBlock;
++ (instancetype)itemWithTitle:(NSString *)title accessibilityIdentifier:(NSString *)accessibilityIdentifier detailTextBlock:(NSString *(^)(void))detailTextBlock selectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))selectBlock;
 + (instancetype)itemWithTitle:(NSString *)title titleDescription:(NSString *)titleDescription accessibilityIdentifier:(NSString *)accessibilityIdentifier detailTextBlock:(id)detailTextBlock selectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))selectBlock;
++ (instancetype)checkmarkItemWithTitle:(NSString *)title selectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))selectBlock;
++ (instancetype)checkmarkItemWithTitle:(NSString *)title titleDescription:(NSString *)titleDescription selectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))selectBlock;
++ (instancetype)checkmarkItemWithTitle:(NSString *)title titleDescription:(NSString *)titleDescription selectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))selectBlock disabledSelectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))disabledSelectBlock;
 + (instancetype)switchItemWithTitle:(NSString *)title switchOn:(BOOL)switchOn switchBlock:(BOOL (^)(YTSettingsCell *, BOOL))switchBlock;
 + (instancetype)switchItemWithTitle:(NSString *)title titleDescription:(NSString *)titleDescription accessibilityIdentifier:(NSString *)accessibilityIdentifier switchOn:(BOOL)switchOn switchBlock:(BOOL (^)(YTSettingsCell *, BOOL))switchBlock settingItemId:(int)settingItemId;
 + (instancetype)switchItemWithTitle:(NSString *)title titleDescription:(NSString *)titleDescription accessibilityIdentifier:(NSString *)accessibilityIdentifier switchOn:(BOOL)switchOn switchBlock:(BOOL (^)(YTSettingsCell *, BOOL))switchBlock selectBlock:(BOOL (^)(YTSettingsCell *, NSUInteger))selectBlock settingItemId:(int)settingItemId;
