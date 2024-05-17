2ed7ac2a29e786edfe7c043a16c8d9bd59cd7454
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-05-17 12:44:16 -0500

    Update YTIPivotBarItemRenderer.h

--- YTIPivotBarItemRenderer.h
+++ YTIPivotBarItemRenderer.h
@@ -2,6 +2,7 @@
 
 @interface YTIPivotBarItemRenderer : NSObject
 @property (nonatomic, copy, readwrite) NSString *targetId;
+@property (nonatomic, copy) NSArray *tabItems;
 - (NSString *)pivotIdentifier;
 - (YTICommand *)navigationEndpoint;
 - (void)setNavigationEndpoint:(YTICommand *)navigationEndpoint;
