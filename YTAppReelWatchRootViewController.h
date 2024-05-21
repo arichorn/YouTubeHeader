21c8a528f5041011c5c9e2ec60c2f446f72917fe
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-05-20 21:29:32 -0500

    Delete YTAppReelWatchRootViewController.h

--- YTAppReelWatchRootViewController.h
+++
@@ -1,8 +0,0 @@
-#import "YTReelHeaderView.h"
-#import "YTResponder.h"
-#import "YTReelHeaderDelegate.h"
-
-@interface YTAppReelWatchRootViewController : UIViewController <YTResponder>
-- (id <YTReelHeaderDelegate>)delegate;
-- (void)didPressVideoQuality:(id)arg;
-@end
