e6f6bd074ee2049a40071991a23acc01dd899a3f
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-06-03 23:59:18 -0500

    Update YTModularPlayerBarView.h

--- YTModularPlayerBarView.h
+++ YTModularPlayerBarView.h
@@ -3,5 +3,5 @@
 @interface YTModularPlayerBarView : UIView
 @property (nonatomic, assign, readonly) CGFloat totalTime;
 @property (nonatomic, assign, readonly) CGFloat mediaTime;
-@property (nonatomic, weak, readwrite) YTMainAppVideoPlayerOverlayViewController *accessibilityDelegate;
+@property (nonatomic, weak, readwrite) id accessibilityDelegate;
 @end
